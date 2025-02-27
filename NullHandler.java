350d4413f8dba13860fdb4c084d32bcebd4d43b1
shreeshail-chavan <shreeshailchavan309@gmail.com>
2025-02-27 06:43:11 +0530

    wrappers and optinal in java

---
+++ NullHandler.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class NullHandler {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<>();
+        map.put("shree", 10);
+        map.put("suraj", 20);
+        Optional<Integer> marks = Optional.ofNullable(map.get("swarup"));
+        if(marks.isPresent())
+            System.out.println(marks.get()+" marks ispresent");
+        else
+            System.out.println("marks got a null value ");
+        
+    }
+}
---
+++ Wrappers.java
@@ -0,0 +1,6 @@
+class Wrappers{
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(Integer.toString(a));
+    }
+}
